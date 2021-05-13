49c3ba6d00bae048ebad6e054e2434c9554d996b
Mountcode <rolopoko@gmail.com>
2021-05-13 14:20:57 +0300

    task 5 complite

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let destrArray = str.split(/[ ,]+/);
+  let minVal = +destrArray[0];
+  let maxVal = minVal;
+
+  for (let i = 0; i < destrArray.length; i++) {
+    let e = +destrArray[i];
+    if (e < minVal) minVal = e;
+    if (e > maxVal) maxVal = e;
+  }
+  return {min: minVal, max: maxVal};
 }
