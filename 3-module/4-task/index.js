c13299c3b11c7ab642eec2b5e908831d2ed13333
Mountcode <rolopoko@gmail.com>
2021-05-13 14:15:17 +0300

    task 4 complite

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultString = '';
+  users.forEach(item =>{
+    if(item.age <= age){
+      let thisName = item.name,
+            thisBalance = item.balance;
+      resultString += `${thisName}, ${thisBalance}\n`
+    }
+  })
+  return resultString.substring(0, resultString.length - 1)
 }
