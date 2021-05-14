3ce04a8ff92618c26532d3b9615068990b8e2d47
Mountcode <rolopoko@gmail.com>
2021-05-14 11:48:00 +0300

    stage 4 task 2 complite

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rowsCount = table.rows.length;
+  
+  for(let i = 0; i < table.rows.length; i++){
+    let currentRow = table.rows[i].cells;
+    for(let g = 0; g < currentRow.length; g++){
+      table.rows[g].cells[g].style.background = 'red' 
+    }
+    break
+  }
 }
