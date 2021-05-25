ab649bcdb0a01c9aaecf89afa4374c5301e7bde9
Mountcode <rolopoko@gmail.com>
2021-05-25 10:20:37 +0300

    stage 6 task 1 complite

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,51 @@
  *      }
  *
  */
-export default class UserTable {
+
+ export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.initTable();
+  }
+  constructData(){
+    let rowsList = '';
+    this.rows.forEach(function(row, index) {
+      rowsList += `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+      `
+    });
+    return rowsList;
+  }
+  initTable(){
+    let table = document.createElement('table');
+    table.innerHTML = `
+        <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Возраст</th>
+                <th>Зарплата</th>
+                <th>Город</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            ${this.constructData()}
+        </tbody>
+    `
+    let buttonList = table.querySelectorAll('button');
+    buttonList.forEach(button =>{
+      button.addEventListener('click', function(){
+        button.closest('tr').remove();
+        this.removeEventListener('click',this)
+      })
+    });
+    return table;
   }
 }
